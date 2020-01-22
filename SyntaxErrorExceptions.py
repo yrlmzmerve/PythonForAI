0d766c025b1f5ba3a9c1fafa5cea30e64b962137
Merve <vier-und.achtzig-tm@hotmail.com>
2020-01-22 17:18:05 +0300

    Update SyntaxErrorExceptions.py

--- SyntaxErrorExceptions.py
+++ SyntaxErrorExceptions.py
@@ -38,4 +38,32 @@ except ZeroDivisionError:
     a = 0
     
     
+# Index Error
+    
+list1=[1,2,3,4]
+list1[15] # list index out of range
 
+# Module not found error
+
+import numppy 
+
+# File not found error
+
+import pandas as pd
+pd.read_csv('asd.csv') # böyle bir dosya yok
+
+# Type error
+"2"+3
+
+#♥ Value error
+int("sad")
+    
+# 
+try:
+    1/0
+except:
+    print("except")
+else:
+    print("else")
+finally:
+    print("done") #her türlü girer
